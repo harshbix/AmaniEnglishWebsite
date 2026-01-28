9301073b6fc55f7e1eebdb87cfd778e884b94371
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2026-01-28 22:40:38 +0300

    Create forms.service.ts

---
+++ backend/src/modules/forms/forms.service.ts
@@ -0,0 +1,15 @@
+import type { AdmissionInquiryPayload, ContactFormPayload } from "../../shared/types/forms.js";
+import { saveAdmissionInquiry, saveContactSubmission } from "./forms.repository.js";
+import { logger } from "../../shared/logger.js";
+
+export const submitContactForm = async (data: ContactFormPayload) => {
+  const submission = await saveContactSubmission(data);
+  logger.info("Contact form submitted", submission);
+  return submission;
+};
+
+export const submitAdmissionInquiry = async (data: AdmissionInquiryPayload) => {
+  const inquiry = await saveAdmissionInquiry(data);
+  logger.info("Admission inquiry submitted", inquiry);
+  return inquiry;
+};
